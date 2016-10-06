7008fd331170464ff8811310f3308216b093c55d
Edson Gustavo Santiago SIlva <edsongus@cs.ualberta.ca>
2016-10-06 15:14:31 -0600

    polls working

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Question
+from .models import Question, Choice
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 from django.http import HttpResponse
@@ -13,8 +15,17 @@ def details(request, question_id):
 	return render(request, 'details.html', {'question': question})
 
 def results(request, question_id):
-	return HttpResponse("You're looking for results of question %s" % (question_id))
+	question = get_object_or_404(Question, pk=question_id)
+	return render(request, 'results.html', {'question': question})
 
 def vote(request, question_id):
-	return HttpResponse("You're voting on question %s" % (question_id))
+	question = get_object_or_404(Question, pk=question_id)
+	try:
+		selected_choice = question.choice_set.get(pk=request.POST['choice'])
+	except (KeyError, Choice.DoesNotExist):
+		return render(request, 'details.html', { 'question': question, 'error_message': "You did not select a choice!"})
+	else:
+		selected_choice.votes += 1
+		selected_choice.save()
+		return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
 
